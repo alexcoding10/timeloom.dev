b8a8715fbae5aad81ab8e0029f0eafadcaa06146
alexcoding10 <alexcoding10@gmail.com>
2025-06-06 12:44:17 +0000

    cambio el domino en las url

--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -1,4 +1,4 @@
 //export const URL_BACKEND_DEV = 'http://192.168.68.100:3001'
 //export const URL_BACKEND_DEV = 'http://localhost:3001'
-export const URL_BACKEND_DEV = 'http://85.219.2.167/api'
-export const URL_WEBSOCKET = 'http://85.219.2.167'
+export const URL_BACKEND_DEV = 'http://timeloomapp.com/api'
+export const URL_WEBSOCKET = 'http://timeloomapp.com'
