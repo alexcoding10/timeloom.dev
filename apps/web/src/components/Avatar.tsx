22fb7b94fc06033c8cc462f43ab36f2ee3deecf5
alexcoding10 <alexcoding10@gmail.com>
2025-06-08 17:27:17 +0200

    mejora visual del chat

--- apps/web/src/components/Avatar.tsx
+++ apps/web/src/components/Avatar.tsx
@@ -6,20 +6,21 @@ type Props = {
     url?: string | null
     connected?:boolean
     width?:string
+    height?:string
 }
 
-export default function Avatar({ name, url,connected,width }: Props) {
+export default function Avatar({ name, url,connected,width,height }: Props) {
     return (
         <div className='flex items-center'>
             {
                 url ? (
                     <img
-                        className={` ${width ? `${width} h-auto` :'w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15'} rounded-full border border-zinc-200  transition-all duration-300 ${connected ? 'ring-2 ring-success-600':''}`}
+                        className={` ${width ? `${width} ${height}` :'w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15'} rounded-full border border-zinc-200  transition-all duration-300 ${connected ? 'ring-2 ring-success-600':''}`}
                         src={`${URL_BACKEND_DEV}${url}`}
                         alt="user"
                     />
                 ) : (
-                    <div className={`w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15 rounded-full bg-neutral-dark-400 border border-zinc-200 flex items-center justify-center  transition-all duration-300 ${connected ? 'ring-2 ring-success-600':''}`}>
+                    <div className={`${width ? `${width} ${height}` :'w-10 h-10 md:w-12 md:h-12 lg:w-15 lg:h-15'} rounded-full bg-neutral-dark-400 border border-zinc-200 flex items-center justify-center  transition-all duration-300 ${connected ? 'ring-2 ring-success-600':''}`}>
                         <p className="text-white font-bold text-lg">
                             {name.substring(0, 2).toUpperCase()}
                         </p>
